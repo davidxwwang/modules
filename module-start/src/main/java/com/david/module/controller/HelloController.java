1fe2148077abd5d1110ff9ed8d7c57bcc78f21cc
david <xwwang_0102@qq.com>
2021-12-27 22:09:09 +0800

    配置一些maven打包的问题

--- module-start/src/main/java/com/david/module/controller/HelloController.java
+++ module-start/src/main/java/com/david/module/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.david.module.controller;
 
+import io.netty.channel.socket.nio.NioChannelOption;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.channels.FileChannel;
+import java.nio.channels.SelectableChannel;
 import java.util.List;
 
 @RestController
